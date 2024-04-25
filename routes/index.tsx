dffc75590d02f2effd59d1b4dcdc35dd794b5af6
Gabriel Lopes Pereira <ogabrielpereira@pm.me>
2024-04-25 20:09:32 -0300

    first commit

---
+++ routes/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <div className="bg-black h-screen w-screen flex flex-col justify-center items-center">
+        <div className="text-white text-3xl flex justify-center">
+          <span>Looking for privacy and freedom. &lt;g@lspr.dev&gt;</span>
+        </div>
+        <div className="text-white text-3xl flex justify-center animate-pulse">
+          <p className="text-white text-xl flex justify-center">Under construction...</p>
+        </div>
+      </div>
+    </>
+  );
+}
